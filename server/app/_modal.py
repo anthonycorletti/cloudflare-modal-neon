45a4250fd46edec2d32ea54bbbd6253efbf4e3d5
Anthony Corletti <anthcor@gmail.com>
2025-04-12 16:07:23 -0400

    modal update

--- server/app/_modal.py
+++ server/app/_modal.py
@@ -1,23 +1,11 @@
-import json
-from typing import Dict
+import os
 
 from fastapi import FastAPI
 from modal import App, Image, Secret, asgi_app
 
-from app.settings import settings
-
 app = App(name="cloudflare-modal-neon")
+app_secrets = [Secret.from_name(f"env-{os.environ['APP_ENV']}")]
 
-_app_env_dict: Dict[str, str | None] = {
-    f"APP_{str(k)}": str(v) for k, v in json.loads(settings.model_dump_json()).items()
-}
-_remove_prefix_keys = ["APP_OPENAI_API_KEY"]
-for key in _remove_prefix_keys:
-    if key in _app_env_dict:
-        new_key = key.replace("APP_", "")
-        _app_env_dict[new_key] = _app_env_dict[key]
-
-app_env = Secret.from_dict(_app_env_dict)
 
 image = (
     Image.debian_slim()
@@ -31,15 +19,7 @@ image = (
 )
 
 
-@app.function(
-    cpu=0.5,
-    memory=256,
-    image=image,
-    timeout=1800,
-    secrets=[app_env],
-    min_containers=1,
-    scaledown_window=30,
-)
+@app.function(image=image, timeout=30, secrets=app_secrets)
 @asgi_app(label="server")
 def server() -> FastAPI:
     from app.main import app
