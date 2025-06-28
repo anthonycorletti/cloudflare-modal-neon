914056690e43469e2b559fdc3332f298bf2c6bdf
Anthony Corletti <anthcor@gmail.com>
2025-06-28 14:17:03 -0400

    test build and deploy step

--- web/src/lib/config.ts
+++ web/src/lib/config.ts
@@ -2,10 +2,12 @@ const env = import.meta.env;
 
 const defaults = {
   ENV: env.MODE,
-  WEB_BASE_URL: env.VITE_WEB_BASE_URL || "https://127.0.0.1:5173",
+  WEB_BASE_URL: env.VITE_WEB_BASE_URL || `${window.location.origin}`,
   SERVER_BASE_URL: env.VITE_SERVER_BASE_URL || "https://127.0.0.1:8000",
   SERVER_CLIENT_TRACE_ROUTE: env.VITE_SERVER_CLIENT_TRACE_ROUTE || "/client-traces",
 };
+
+console.log("CONFIG", defaults);
 
 export const CONFIG = {
   ...defaults,
