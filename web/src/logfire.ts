52505af361e15ac028eab38c1a2a5d2acb8af7ee
Anthony Corletti <anthcor@gmail.com>
2025-07-07 21:16:56 -0400

    updates

--- web/src/logfire.ts
+++ web/src/logfire.ts
@@ -3,36 +3,43 @@ import * as logfire from '@pydantic/logfire-browser';
 import packageJson from '../package.json';
 import { CONFIG } from "./lib/config";
 
-logfire.configure({
-    // environment: CONFIG.ENV, -- TODO: logfire-browser does not support environment yet
-    traceUrl: CONFIG.SERVER_BASE_URL + CONFIG.SERVER_CLIENT_TRACE_ROUTE,
-    serviceName: `web-${CONFIG.ENV}`,
-    serviceVersion: packageJson.version,
-    // The instrumentations to use
-    // https://www.npmjs.com/package/@opentelemetry/auto-instrumentations-web - for more options and configuration
-    instrumentations: [
-        getWebAutoInstrumentations()
-    ],
-    // This outputs details about the generated spans in the browser console, use only in development and for troubleshooting.
-    diagLogLevel: (CONFIG.ENV === 'development') ? logfire.DiagLogLevel.ALL : logfire.DiagLogLevel.WARN,
-})
+// if config.env is development pass
+if (CONFIG.ENV === 'development') {
+    console.warn("Logfire is not initialized in development mode. Set CONFIG.ENV to 'production' to enable logging.");
+} else {
 
-window.addEventListener("error", (event) => {
-    logfire.error("Uncaught error", {
-        message: event.message,
-        stack: event.error?.stack,
-        error: event.error,
-        filename: event.filename,
-        lineno: event.lineno,
-        colno: event.colno,
+    logfire.configure({
+        environment: CONFIG.ENV,
+        traceUrl: CONFIG.SERVER_BASE_URL + CONFIG.SERVER_CLIENT_TRACE_ROUTE,
+        serviceName: `web-${CONFIG.ENV}`,
+        serviceVersion: packageJson.version,
+        // The instrumentations to use
+        // https://www.npmjs.com/package/@opentelemetry/auto-instrumentations-web - for more options and configuration
+        instrumentations: [
+            getWebAutoInstrumentations()
+        ],
+        // This outputs details about the generated spans in the browser console, use only in development and for troubleshooting.
+        diagLogLevel: (CONFIG.ENV === 'development') ? logfire.DiagLogLevel.ALL : logfire.DiagLogLevel.WARN,
+    })
+
+    window.addEventListener("error", (event) => {
+        logfire.error("Uncaught error", {
+            message: event.message,
+            stack: event.error?.stack,
+            error: event.error,
+            filename: event.filename,
+            lineno: event.lineno,
+            colno: event.colno,
+        });
     });
-});
 
-window.addEventListener("unhandledrejection", (event) => {
-    logfire.error("Unhandled promise rejection", {
-        reason: event.reason,
-        stack: event.reason?.stack,
+    window.addEventListener("unhandledrejection", (event) => {
+        logfire.error("Unhandled promise rejection", {
+            reason: event.reason,
+            stack: event.reason?.stack,
+        });
     });
-});
 
-logfire.error("Logfire initialized! 🔥");
+    logfire.error("Logfire initialized! 🔥");
+
+}
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,16 @@
+import tailwindcss from '@tailwindcss/vite';
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 
+
 export default defineConfig(({ mode }) => {
   let pluginArr = [];
   if (mode === "development") {
-    pluginArr = [react(), basicSsl()];
+    pluginArr = [react(), tailwindcss(), basicSsl()];
   } else {
-    pluginArr = [react()];
+    pluginArr = [react(), tailwindcss()];
   }
   const plugins = pluginArr;
   return {
